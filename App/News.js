451c9b4ed5630a18a5dd4e5c7d73ddaf5821c6a8
Sang Min Na <xsangmin@gmail.com>
2016-12-12 16:17:06 -0500

    update

--- App/News.js
+++ App/News.js
@@ -9,17 +9,21 @@ import {
 } from 'react-native';
 
 export default class News extends Component {
+  handClick(url) {
+    console.log('********** url', url)
+  }
+
   renderArticles() {
     const articles = this.props.articles;
     return this.props.articles.map((article, i) => {
     // return articles.map((article, i) => {
       return (
-        <View key={i}>
+        <TouchableOpacity key={i} style={styles.articleView} onPress={() => this.handClick(article.url)}>
           <Image source={{uri: article.urlToImage}} style={styles.articleImg}/>
           <Text>{article.title}</Text>
           <Text>{article.description}</Text>
           <Text>{article.author}</Text>
-        </View>
+        </TouchableOpacity>
       );
     });
   }
@@ -37,17 +41,23 @@ const styles = StyleSheet.create({
   articleView: {
     backgroundColor: 'orange',
     padding: 20,
+    margin: 5,
   },
   articleTitle: {
     fontSize: 25,
     color: 'blue',
+    width: 50,
+    flex: 1,
   },
   articleText: {
     color: 'red',
+    width: 50,
+    flex: 1,
   },
   articleImg: {
-    width: 50,
-    height: 50,
+    width: 100,
+    height: 100,
     margin: 5,
+    flex: 2,
   },
 });
